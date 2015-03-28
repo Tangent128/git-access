c8b605a8f7e76c8d3e6374ac9ae0e378d88c8d08
Tangent 128 <Tangent128@gmail.com>
2015-03-28 19:49:28 -0400

    * add script to generate the entry-point file that can be safely referenced from authorized_keys

---
+++ generateEntryScript.lua
@@ -0,0 +1,37 @@
+
+function die(msg)
+	io.stderr:write(msg.."\n")
+	os.exit(1)
+end
+
+function which(cmd)
+	local file = io.popen("which "..cmd, "r")
+	local path = assert(file:read())
+	file:close()
+	return path
+end
+
+function printTable(t)
+	for k,v in pairs(t) do
+		print(k, v)
+	end
+end
+
+local scriptTmpl = [[
+#!%s
+
+package.path = "%s"
+package.cpath = "%s"
+
+require "git-ssh-access" {
+	upload = "%s",
+	receive = "%s",
+	...
+}]]
+
+local lua = which(arg[-1])
+local upload = which "git-upload-pack"
+local receive = which "git-receive-pack"
+local script = scriptTmpl:format(lua, package.path, package.cpath, upload, receive)
+
+print(script)
